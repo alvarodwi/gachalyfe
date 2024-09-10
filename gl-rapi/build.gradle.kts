8fe96ffe42c28bf0b72f774e38bfe8c815056029
Alvaro Dwi <varoa.dwi@gmail.com>
2024-09-11 00:46:43 +0700

    feat: adding detekt and versions
    for code smell and checking deps version
    but now i kinda regret adding map struct in the project
    after updating kotlin to 2.0.20, i realize that kapt will not be supported in favor of k2
    i'm gonna revert it back to extension function

--- gl-rapi/build.gradle.kts
+++ gl-rapi/build.gradle.kts
@@ -1,11 +1,15 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("kapt") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    kotlin("plugin.jpa") version "1.9.25"
+    kotlin("jvm") version "2.0.20"
+    kotlin("kapt") version "2.0.20"
+    kotlin("plugin.spring") version "2.0.20"
+    kotlin("plugin.jpa") version "2.0.20"
     id("org.springframework.boot") version "3.3.3"
     id("io.spring.dependency-management") version "1.1.6"
     id("org.jmailen.kotlinter") version "4.4.1"
+    id("io.gitlab.arturbosch.detekt") version "1.23.7"
+    id("com.github.ben-manes.versions") version "0.51.0"
 }
 
 group = "me.gachalyfe"
@@ -67,4 +71,28 @@ tasks.withType<Jar>() {
     enabled = true
     archiveClassifier = ""
     archiveFileName.set("${project.name}.jar")
+}
+
+// ben-manes versions checking
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any {
+        version.uppercase().contains(it)
+    }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    // reject all non stable versions
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
+    gradleReleaseChannel = "current"
+
+    // optional parameters
+    checkForGradleUpdate = true
+    outputFormatter = "json"
+    outputDir = "build/dependencyUpdates"
+    reportfileName = "report"
 }
